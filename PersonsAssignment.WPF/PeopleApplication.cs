0bf26ef86c30c31a1f009f222d7cf11b114dca85
Ewout Eetesonne <ewout.eetesonne@hogent.be>
2023-12-07 21:17:14 +0100

    Added people overview window

--- PersonsAssignment.WPF/PeopleApplication.cs
+++ PersonsAssignment.WPF/PeopleApplication.cs
@@ -5,9 +5,14 @@ namespace PersonsAssignment.WPF
 	public class PeopleApplication
 	{
 		private readonly DomainManager _domainManager;
+		private PeopleWindow _peopleWindow;
 		public PeopleApplication(DomainManager manager)
 		{
 			_domainManager = manager;
+			_peopleWindow = new();
+			_peopleWindow.Show();
+
+			_peopleWindow.People = _domainManager.GetAllPersons();
 		}
 	}
 }
---
+++ PersonsAssignment.WPF/PeopleWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace PersonsAssignment.WPF
+{
+	/// <summary>
+	/// Interaction logic for PeopleWindow.xaml
+	/// </summary>
+	public partial class PeopleWindow : Window
+	{
+		public List<string> People { set => PeopleListBox.ItemsSource = value; }
+		public PeopleWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
