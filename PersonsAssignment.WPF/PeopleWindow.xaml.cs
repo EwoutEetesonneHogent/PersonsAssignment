843a8ef41a47902eadf51a3184b419f3c55ba4a4
AnckBart <bart.anckaert@student.hogent.be>
2023-12-10 13:13:32 +0100

    made method to get id from selected person multi use

--- PersonsAssignment.WPF/PeopleWindow.xaml.cs
+++ PersonsAssignment.WPF/PeopleWindow.xaml.cs
@@ -14,8 +14,9 @@ namespace PersonsAssignment.WPF
 	
 		public event EventHandler AddingPerson;
 		public event EventHandler<PersonIdArgs> RemoveingPerson;
+        public event EventHandler<PersonIdArgs> EditPerson;
 
-		public PeopleWindow()
+        public PeopleWindow()
 		{
 			InitializeComponent();
 		}
@@ -27,34 +28,39 @@ namespace PersonsAssignment.WPF
 
         private void PeopleListBox_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-			MessageBox.Show("you doubleclicked someone");
+            int id = GetIdFromSelectedPerson();
+            if (id > 0) EditPerson?.Invoke(this, new(id));
         }
 
         private void btnDeletePerson_Click(object sender, RoutedEventArgs e)
         {
-			if (PeopleListBox.SelectedIndex < 0 ) // No selection = -1
-			{
-                MessageBox.Show($"you have to select a person to delete!");
-            } else
-			{
-				int id = GetIdFromSelection(PeopleListBox.SelectedItem.ToString());
-                if (id>0) RemoveingPerson?.Invoke(this, new(id));
-            }
+			int id = GetIdFromSelectedPerson();
+            if (id>0) RemoveingPerson?.Invoke(this, new(id));
+            
         }
 
-		private int GetIdFromSelection(string input)
+		private int GetIdFromSelectedPerson()
 		{
-			try 
-			{
-                int startIndex = input.IndexOf('[');
-                int endIndex = input.IndexOf(']');
-                return int.Parse(input.Substring(startIndex + 1, endIndex - startIndex - 1));
-            }catch(Exception)
-			{
-				MessageBox.Show($"could not get Person Id from the selected person: \n{input}");
-				return -1;
-			}
-			
+            if (PeopleListBox.SelectedIndex < 0) // No selection = -1
+            {
+                MessageBox.Show($"you have to select a person!");
+                return -1;
+            }
+            else
+            {
+                try
+                {
+                    string input = PeopleListBox.SelectedItem.ToString();
+                    int startIndex = input.IndexOf('[');
+                    int endIndex = input.IndexOf(']');
+                    return int.Parse(input.Substring(startIndex + 1, endIndex - startIndex - 1));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show($"could not get Person Id from the selected person: \n{input}");
+                    return -1;
+                }
+            }
         }
  
     }
