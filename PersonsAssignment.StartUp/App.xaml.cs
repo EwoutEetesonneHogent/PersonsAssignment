d11bdc92bd59bbec5894b2782995bffeeae26fe4
Ewout Eetesonne <ewout.eetesonne@hogent.be>
2023-12-12 20:16:29 +0100

    Added DispatcherUnhandledException

--- PersonsAssignment.StartUp/App.xaml.cs
+++ PersonsAssignment.StartUp/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using PersonsAssignment.Database;
 using PersonsAssignment.Domain;
 using PersonsAssignment.WPF;
+using System;
 using System.Windows;
 
 namespace PersonsAssignment.StartUp
@@ -12,11 +13,17 @@ namespace PersonsAssignment.StartUp
 	{
 		private void Application_Startup(object sender, StartupEventArgs e)
 		{
-			PeopleMapper mapper = new PeopleMapper();
+				PeopleMapper mapper = new PeopleMapper();
 
-			DomainManager manager = new DomainManager(mapper);
+				DomainManager manager = new DomainManager(mapper);
 
-			PeopleApplication application = new(manager);
+				PeopleApplication application = new(manager);
+		}
+
+		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+		{
+			MessageBox.Show("Something went terribly wrong", e.Exception.Message);
+			Application.Current.Shutdown();
 		}
 	}
 }
